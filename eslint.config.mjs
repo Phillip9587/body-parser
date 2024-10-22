0ae421db49dcfb53d6155d31171091899556df1f
Phillip9587 <barta.phillip@gmail.com>
2024-10-22 21:47:51 +0200

    WIP

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import markdown from '@eslint/markdown';
+
+export default [
+  { ignores: ['coverage/*'] },
+  ...markdown.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2015,
+      sourceType: 'commonjs',
+      globals: globals.node
+    },
+    rules: js.configs.recommended.rules
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: globals.mocha
+    },
+    rules: {
+      'no-unused-vars': 'off'
+    }
+  },
+];
